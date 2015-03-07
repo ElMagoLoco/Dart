aef3c5c1d7833c2a44bea0510fa314a7ced1c1e9
manny1sfresh <mesa.gamecoder@gmail.com>
2015-03-07 09:55:59 -0500

    Audio
    Music add to content file

---
+++ Dart/Audio.cpp
@@ -0,0 +1,30 @@
+#include "Audio.h"
+
+
+//Music Files
+Audio::Audio()
+{
+	//HUB music
+};
+void Audio::Background_Music()
+{
+	//Background music
+};
+
+
+
+//Sound Effects Files
+void Audio::Antz_sfx()
+{
+	//Ants walking
+};
+void Audio::LowHealth_sfx()
+{
+	//Low health
+};
+void Audio::Attack_sfx()
+{
+	//Attack
+};
+
+
---
+++ Dart/Audio.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <Windows.h>
+#include <mmsystem.h>
+#include <iostream>
+
+class Audio
+{
+public:
+
+	Audio();
+	void Background_Music();
+	void Attack_sfx();
+	void LowHealth_sfx();
+	void Antz_sfx();
+	void Mesh2_sfx();
+
+
+
+protected:
+
+};
+
